ce360a5c06230da809e92aaea629cfbfdedd77fe
Satvir Sandhu <356Sandhu@gmail.com>
2020-09-03 16:45:18 -0400

    componentize pages with RReact Router and add Search page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Fragment } from "react";
 import MovieGallery from "./components/MovieGallery";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Fragment>
-      <Home />
-    </Fragment>
+    <Router>
+      <header>
+        <nav>
+          <div className="container">
+            <ul>
+              <li>
+                <Link to="/"> The Shoppies </Link>
+              </li>
+              <li>
+                built and designed by <strong>satvir sandhu</strong>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search">
+            <h1>Haha, welcome to the Search page.</h1>
+          </Route>
+        </Switch>
+      </header>
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,20 @@
 import React from "react";
 import MovieGallery from "../components/MovieGallery";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <header>
-        <nav>
-          <div className="container">
-            <ul>
-              <li>The Shoppies</li>
-              <li>
-                built and designed by <strong>satvir sandhu</strong>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="content container">
-          <div className="left">
-            <h1>Celebrate Cinema</h1>
-            <p>Nominate your favourite movies for The Shoppies.</p>
-            <button>Get Started</button>
-          </div>
-          <div className="right">
-            <MovieGallery />
-          </div>
-        </div>
-      </header>
+    <div className="content container">
+      <div className="left">
+        <h1>Celebrate Cinema</h1>
+        <p>Nominate your favourite movies for The Shoppies.</p>
+        <Link to="/search" className="navToSearch">
+          Get Started
+        </Link>
+      </div>
+      <div className="right">
+        <MovieGallery />
+      </div>
     </div>
   );
 };
